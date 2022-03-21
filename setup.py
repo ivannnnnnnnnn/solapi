5e11e7792c3100a59a711db94d23737db17897e0
Ivan Siroshtan <ivan.siroshtan@onlystudio.org>
2022-03-21 21:18:27 +0200

    update readme, up version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='solapi',
     packages=find_packages(),
-    version='0.0.4',
+    version='0.0.5',
     description='Python wrap for solana NFT marketplaces such as MagicEden and Solanart',
     author='ivan.srshtn.crypto@gmail.com',
     license='MIT',
